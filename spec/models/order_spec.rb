6740c154e96befe935ff3e1b8422601829ef1f85
Alexandre Moraes Tannus <alexandretannus@iMac-de-Alexandre.local>
2022-12-01 16:39:56 -0300

    :white_check_mark: Adição de teste com lista

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Order, type: :model do
 
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it 'Tem 3 pedidos' do
+    orders = create_list(:order, 3)
+
+    expect(orders.count).to eq(3)
+  end
 end
