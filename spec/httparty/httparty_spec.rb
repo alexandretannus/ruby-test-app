d5b35261a7ced53dff4524967aca1f47190cb94e
Alexandre Moraes Tannus <alexandretannus@iMac-de-Alexandre.local>
2022-12-05 09:59:38 -0300

    :white_check_mark: Adição teste com  HTTParty e VCR

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,12 +1,14 @@
 describe 'HTTParty' do
     it 'httparty' do
-        stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
-            .to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
-        
-        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-        
-        contentType = response.headers['content-type']
-        
-        expect(contentType).to include('application/json')
+        # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
+        #     .to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+        VCR.use_cassette("jsonplaceholder/posts") do
+            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+            
+            contentType = response.headers['content-type']
+            
+            expect(contentType).to include('application/json')
+            
+        end
     end
 end
