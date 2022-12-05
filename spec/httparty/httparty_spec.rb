217cc2aaf37f46bc0c67545657135286ad609dad
Alexandre Moraes Tannus <alexandretannus@iMac-de-Alexandre.local>
2022-12-05 09:14:07 -0300

    :white_check_mark: Adição do webmock no teste do HTTParty

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,7 +1,10 @@
 describe 'HTTParty' do
     it 'httparty' do
+        stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
+            .to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+        
         response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-
+        
         contentType = response.headers['content-type']
         
         expect(contentType).to include('application/json')
